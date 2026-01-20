34b354408d6ef9f4183630a4b7f13c71f8c5874d
Charles Martin <charleslmartin4@gmail.com>
2026-01-20 15:03:49 +0100

    meal_plan model and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_19_160509) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_20_134211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meal_plans", force: :cascade do |t|
+    t.string "plan_title"
+    t.string "goal"
+    t.string "additional_preferences"
+    t.string "system_prompt"
+    t.string "diet"
+    t.string "meals_required"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_meal_plans_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_19_160509) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "meal_plans", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# create_table "meal_plans", force: :cascade do |t|
+#     t.string "plan_title"
+#     t.string "goal"
+#     t.string "additional_preferences"
+#     t.string "system_prompt"
+#     t.string "diet"
+#     t.string "meals_required"
+#     t.bigint "user_id", null: false
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.index ["user_id"], name: "index_meal_plans_on_user_id"
+#   end
+
+user = User.new(
+  email: "email@email.com",
+  password: "123456"
+)
+
+user.save!
+
+meal_plan = MealPlan.new(
+  user: user,
+  plan_title: "Melissa's plan",
+  goal: "more muscle",
+  additional_preferences: "no eggs",
+  system_prompt: "placeholder",
+  meals_required: "no breakfast, 5 days per week lunch and dinner",
+  diet: "omni"
+)
+
+meal_plan.save!
