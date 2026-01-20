2b160f3770bd46fc3bfd160a9d32a683dbc3477a
Melissa Laurenti <melissa.laurenti@outlook.com>
2026-01-20 17:12:44 +0100

    chat.rb messages.rb

---
+++ app/models/chat.rb
@@ -0,0 +1,11 @@
+class Chat < ApplicationRecord
+  belongs_to :user
+  belongs_to :meal_plan
+  belongs_to :profile_information
+
+  has_many :messages
+
+  validates :user_id, presence: true
+  valitades :profile_information, presence: true
+  validates :meal_plan, presence: true
+end
