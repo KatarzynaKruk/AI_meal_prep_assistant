05f92fad2840ce0430a0b0de9e5015d6f3f4ac3a
Charles Martin <charleslmartin4@gmail.com>
2026-01-20 18:21:04 +0100

    fixed functionality of meal_plans#create

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -1,24 +1,24 @@
 class MealPlansController < ApplicationController
-
+  before_action :set_user
   # resources :meal_plans, only: [:create, :index, :show]
 
   def index
-    @meal_plans = current_user.meal_plans
+    @meal_plans = @user.meal_plans
   end
 
   def new
-    @meal_plan = current_user.meal_plans.build()
-    @profile_information = current_user.profile_information
+    @meal_plan = @user.meal_plans.build()
+    @profile_information = @user.profile_information
   end
 
   def create
-    @profile_information = current_user.profile_information
-    @meal_plan = current_user.meal_plans.build(meal_plan_params.except(:system_prompt))
+    @profile_information = @user.profile_information
+    @meal_plan = @user.meal_plans.build(meal_plan_params.except(:system_prompt))
 
     @meal_plan.system_prompt = build_system_prompt(@meal_plan, @profile_information)
 
     if @meal_plan.save
-      redirect_to meal_plans_path
+      redirect_to user_meal_plans_path(@user)
     else
       render :new, status: :unprocessable_entity
     end
@@ -54,17 +54,16 @@ class MealPlansController < ApplicationController
     age = Date.current.year - profile.date_of_birth.year
 
     system_prompt =
-    "You are a experienced nutritionist helping me with meal planning.
-    I am a #{age} year old #{profile.gender}.
-    My goal is to #{meal_plan.goal} and my diet is #{meal_plan.diet}.
-    My current weight is #{profile.weight} and my height is #{profile.height}.
-    #{plan_restrictions_text}.
-    Additionally #{meals_required_text} and #{additional_preferences_text}.
-    Return a concise meal plan and supporting shopping list.  The meal plan should be segregated by days and meals.
-    The shopping list should have clearly marked what recipe each ingredient is intended for and be segregated by common supermarket sections.
-    The return should be in markdown format.
-    Additionally ask me afterwards if there are any further adjustments to the plan I would like based on the output you provide.
-    "
+      "You are a experienced nutritionist helping me with meal planning.
+      I am a #{age} year old #{profile.gender}.
+      My goal is to #{meal_plan.goal} and my diet is #{meal_plan.diet}.
+      My current weight is #{profile.weight} and my height is #{profile.height}.
+      #{plan_restrictions_text}.
+      Additionally #{meals_required_text} and #{additional_preferences_text}.
+      Return a concise meal plan and supporting shopping list.  The meal plan should be segregated by days and meals.
+      The shopping list should have clearly marked what recipe each ingredient is intended for and be segregated by common supermarket sections.
+      The return should be in markdown format.
+      Additionally ask me afterwards if there are any further adjustments to the plan I would like based on the output you provide."
   end
 
   private
@@ -80,6 +79,8 @@ class MealPlansController < ApplicationController
     )
   end
 
-
+  def set_user
+    @user = current_user
+  end
 
 end
