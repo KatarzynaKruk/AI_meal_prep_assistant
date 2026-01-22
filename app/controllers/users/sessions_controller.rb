d352d7ed489eee3eb081d3c84369511a736872d9
Charles Martin <charleslmartin4@gmail.com>
2026-01-22 14:29:05 +0100

    quick bug fix

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -20,6 +20,10 @@ class Users::SessionsController < Devise::SessionsController
 
   protected
 
+def after_sign_in_path_for(resource)
+  user_meal_plans_path(current_user)
+end
+
 def after_sign_out_path_for(resource)
   root_path
 end
