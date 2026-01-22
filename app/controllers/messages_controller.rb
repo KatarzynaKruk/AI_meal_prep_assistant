73d16fd4defbb25b048f17acc20044da3253de05
Charles Martin <charleslmartin4@gmail.com>
2026-01-22 12:35:31 +0100

    fixed delete issue, fixed order of meal plan chat, added some small front end improvements to chats#show

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,19 +2,6 @@ class MessagesController < ApplicationController
   before_action :set_user
   before_action :set_chat
 
-  # SYSTEM_PROMPT = @user.meal_plans.system_prompt
-  # def index
-  #   @chats = current_user.chats.includes(:meal_plan, :profile_information)
-  # end
-
-  # def new
-  #   @chat = Chat.new
-  # end
-
-  # def show
-  #   render @chat, status: :ok
-  # end
-
   def create
     # @chat = current_user.chats.find(params[:chat_id])
     # @meal_plan = @chat.meal_plan
@@ -22,18 +9,31 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chat = @chat
     @message.role = "user"
+
     if @message.save
       ruby_llm_chat = RubyLLM.chat
+      build_conversation_history(ruby_llm_chat)
       response = ruby_llm_chat.with_instructions(@meal_plan.system_prompt).ask(@message.content)
       Message.create(role: "assistant", content: response.content, chat: @chat)
+      # @chat.generate_title_from_first_message
       # Message.create(content: response.content, chat: @chat)
       redirect_to user_meal_plan_chat_path(@meal_plan.user, @chat)
     else
+       @messages = @chat.messages.order(created_at: :desc)
       render "chats/show", status: :unprocessable_entity
     end
   end
 
   private
+
+  def build_conversation_history(ruby_llm_chat)
+    @chat.messages.each do |message|
+      ruby_llm_chat.add_message(
+        role: message.role.to_sym,
+        content: message.content
+        )
+    end
+  end
 
   def set_user
     @user = current_user
@@ -43,6 +43,7 @@ class MessagesController < ApplicationController
     @meal_plan = MealPlan.find(params[:meal_plan_id])
     @chat = @meal_plan.chat || @meal_plan.build_chat
     @chat.save! if @chat.new_record?
+    @messages = @chat.messages.order(created_at: :desc)
   end
 
   def message_params
