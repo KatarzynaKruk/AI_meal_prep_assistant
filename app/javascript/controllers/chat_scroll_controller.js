be93c9fd9c27cf255623fe7295a1764fdc379ddd
Charles Martin <charleslmartin4@gmail.com>
2026-01-23 15:23:16 +0100

    fixed scroll

--- app/javascript/controllers/chat_scroll_controller.js
+++ app/javascript/controllers/chat_scroll_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.scrollTop = this.element.scrollHeight
+    this.element.scrollTop = 0
   }
 }
